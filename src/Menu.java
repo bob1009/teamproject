5ce08f157f12f00451a7dd2e6bc08b2b7c61a5d0
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-02-12 17:39:52 -0800

    addedrandomnumber

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class Menu {
@@ -19,7 +21,7 @@ public class Menu {
 
 		}
 		if (operation == 3) {
-			System.out.println("generate numbers ");
+		random();
 
 		}
 	}
@@ -33,4 +35,16 @@ public class Menu {
 			JOptionPane.showMessageDialog(null, "you are wrong");
 		}
 	}
+	
+	public static void random() {
+		String b = JOptionPane.showInputDialog(null, " name a number. Any number");
+
+		int i = Integer.parseInt(b);
+
+		Random t = new Random();
+		int c = t.nextInt(i);
+		JOptionPane.showMessageDialog(null, "your random number is " + c);
+
+	}
+	
 }
